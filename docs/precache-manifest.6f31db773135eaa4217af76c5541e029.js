ffdbe15297d304008aa894ae37b9fb4ac7d7423a
azhu98 <43658543+azhu98@users.noreply.github.com>
2020-02-13 23:29:57 -0800

    my portfolio1

---
+++ docs/precache-manifest.6f31db773135eaa4217af76c5541e029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fbecb769e864ac0fa4121b666ea416",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "691c1b2d3245f0302e91",
+    "url": "/portfolio/static/css/main.3d6bb6ec.chunk.css"
+  },
+  {
+    "revision": "3f7c1abb021fc6586ab9",
+    "url": "/portfolio/static/js/2.9bc43008.chunk.js"
+  },
+  {
+    "revision": "691c1b2d3245f0302e91",
+    "url": "/portfolio/static/js/main.c797b1ba.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
